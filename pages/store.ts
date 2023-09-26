7971bfe518d2160a6f52e715620d44a2e33a2fae
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-26 20:10:09 +0200

    first commit

---
+++ pages/store.ts
@@ -0,0 +1,33 @@
+import { computed, makeObservable, observable } from "mobx";
+
+export interface Blog {
+    id: number,
+    title: string,
+    body: string,
+}   
+
+class BlogStore {
+    blog: Blog[] = [];
+    filter: string = "";
+    constructor() {
+        makeObservable(this, {
+            blog: observable,
+            filter: observable,
+            filteredBlog: computed,
+        })
+    }
+
+    setBlog(blog: Blog[]) {
+        this.blog = blog;
+    }
+
+    get filteredBlog() {
+        return this.blog.filter(({ title }) =>
+          title.toLowerCase().includes(this.filter.toLowerCase())
+        );
+      }
+}
+
+const store = new BlogStore();
+
+export default store;
